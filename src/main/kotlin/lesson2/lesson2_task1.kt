8634d326292d72400ef670ce6ae03dfe44916e36
miroks404 <themiroks@gmail.com>
2024-10-02 17:58:34 +0200

    KS-2-1 class average

---
+++ src/main/kotlin/lesson2/lesson2_task1.kt
@@ -0,0 +1,13 @@
+package org.example.lesson2
+
+fun main() {
+    val students = 4
+    val firstGrade = 3f
+    val secondGrade = 4f
+    val thirdGrade = 3f
+    val fourthGrade = 5f
+
+    val classAverage = (firstGrade + secondGrade + thirdGrade + fourthGrade) / students
+
+    println(classAverage)
+}
