caf1479fe755f51ec133c67f5f901666e1c6c3c2
miroks404 <themiroks@gmail.com>
2024-10-03 00:50:52 +0200

    KS-2-3 time of arrival

---
+++ src/main/kotlin/lesson2/lesson2_task3.kt
@@ -0,0 +1,14 @@
+package org.example.lesson2
+
+fun main() {
+    val departureHour = 9
+    val departureMinute = 39
+    val minutesOnTheWay = 457
+    var minuteOfArrival = departureMinute + minutesOnTheWay
+    var hourOfArrival = departureHour
+    while (minuteOfArrival >= 60) {
+        hourOfArrival += 1
+        minuteOfArrival -= 60
+    }
+    println("Время прибытия: $hourOfArrival:$minuteOfArrival")
+}
