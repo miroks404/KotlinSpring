52296cd7999fed07493190d0f7eaef1006597da9
miroks404 <themiroks@gmail.com>
2024-10-02 18:26:29 +0200

    KS-2-2 accounting

---
+++ src/main/kotlin/lesson2/lesson2_task2.kt
@@ -0,0 +1,18 @@
+package org.example.lesson2
+
+fun main (){
+    val staff:Short = 50
+    val salaryStaff = 30000
+
+    val trainee = 30
+    val salaryTrainee = 20000
+
+    val expensesOfSalaryStaff = staff * salaryStaff
+    val totalExpenses = (staff * salaryStaff) + (trainee * salaryTrainee)
+
+    val totalAverageSalary = totalExpenses / (staff + trainee)
+
+    println("Расходы на выплату зарплаты постоянных сотрудников: $expensesOfSalaryStaff")
+    println("Общие расходы по ЗП после прихода стажеров: $totalExpenses")
+    println("Средняя ЗП одного сотрудника после устройства стажеров: $totalAverageSalary")
+}
