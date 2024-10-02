a0c4afc217d451c849cbfe274975973fce60ead2
miroks404 <themiroks@gmail.com>
2024-10-02 17:54:46 +0200

    KS-1-4 default types of variables

---
+++ src/main/kotlin/lesson1/lesson1_task4.kt
@@ -0,0 +1,17 @@
+package org.example.lesson1
+
+fun main() {
+    val lenght: ULong = 40868600000u
+    val age: UByte = 27u
+    val partOfDay: Float = 0.075f
+    val seconds: UShort = 6480u
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: UInt = 327000u
+
+    println("Расстояние: $lenght")
+    println("Возраст: $age")
+    println("Часть дня: $partOfDay")
+    println("Секунды: $seconds")
+    println("Часть года: $partOfYear")
+    println("Апогей орбиты: $apogee")
+}
