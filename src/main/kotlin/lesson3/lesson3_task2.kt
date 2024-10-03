a4780991894e3afdc6e5cf0d2d23f4620c3db087
miroks404 <themiroks@gmail.com>
2024-10-03 22:26:19 +0200

    KS-3-2 full name

---
+++ src/main/kotlin/lesson3/lesson3_task2.kt
@@ -0,0 +1,17 @@
+package org.example.lesson3
+
+fun main() {
+    var secondName = "Андреева"
+    val firstName = "Татьяна"
+    val surname = "Сергеевна"
+    var age = 20
+
+    println("ФИО и возраст: $secondName $firstName $surname, $age")
+
+    //Выход замуж
+
+    secondName = "Сидорова"
+    age = 22
+
+    println("ФИО и возраст: $secondName $firstName $surname, $age")
+}
