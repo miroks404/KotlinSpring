8d48e307c7694697275b288e7b151c3e3e3a5bc6
miroks404 <themiroks@gmail.com>
2024-10-03 21:47:04 +0200

    KS-1-5 corrections

--- src/main/kotlin/lesson1/lesson1_task5.kt
+++ src/main/kotlin/lesson1/lesson1_task5.kt
@@ -1,24 +1,16 @@
 package org.example.lesson1
 
 fun main() {
-    var secondsOfFlight = 5409
-    var minutesOfFlight = 0
-    var hoursOfFlight = 0
-    while (secondsOfFlight > 60) {
-        minutesOfFlight += 1
-        secondsOfFlight -= 60
-    }
-    while (minutesOfFlight > 60) {
-        hoursOfFlight += 1
-        minutesOfFlight -= 60
-    }
-    var resultSecondsOfFlight = String.format("%d", secondsOfFlight)
-    var resultMinutesOfFlight = String.format("%d", minutesOfFlight)
-    var resultHoursOfFlight = String.format("%d", hoursOfFlight)
-    if (secondsOfFlight <= 9) resultSecondsOfFlight = String.format("%02d", secondsOfFlight)
-    if (minutesOfFlight <= 9) resultMinutesOfFlight = String.format("%02d", minutesOfFlight)
-    if (hoursOfFlight <= 9) resultHoursOfFlight = String.format("%02d", hoursOfFlight)
-    println("$resultHoursOfFlight:$resultMinutesOfFlight:$resultSecondsOfFlight")
-    //На последок хочу сказать, что я пытался этот код сократить воспользываясь разными методами
-    //Но, к сожалению, за час поисков ничего лучше не нашел((( Ну хоть этот работает и в нем можно изменять время :O
+    var secondsOfFlight = 6480
+    var minutesOfFlight: Int
+    val hoursOfFlight: Int
+
+    minutesOfFlight = secondsOfFlight / SECONDS_PER_MINUTE_AND_MINUTES_PER_HOUR
+    hoursOfFlight = minutesOfFlight / SECONDS_PER_MINUTE_AND_MINUTES_PER_HOUR
+    minutesOfFlight %= SECONDS_PER_MINUTE_AND_MINUTES_PER_HOUR
+    secondsOfFlight %= SECONDS_PER_MINUTE_AND_MINUTES_PER_HOUR
+
+    println(String.format("%02d:%02d:%02d", hoursOfFlight, minutesOfFlight, secondsOfFlight))
 }
+
+const val SECONDS_PER_MINUTE_AND_MINUTES_PER_HOUR = 60
