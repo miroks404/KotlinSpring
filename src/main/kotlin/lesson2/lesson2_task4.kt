4697f8d23642b1e7f68000bb64b28bc8311c837c
miroks404 <themiroks@gmail.com>
2024-10-03 21:22:30 +0200

    KS-2-4 corrections

--- src/main/kotlin/lesson2/lesson2_task4.kt
+++ src/main/kotlin/lesson2/lesson2_task4.kt
@@ -3,9 +3,10 @@ package org.example.lesson2
 fun main() {
     val ironOre = 11
     val crystalOre = 7
-    val buff = 0.2f
-    val bonusIronOre = ironOre * buff
-    val bonusCrystalOre = crystalOre * buff
+    val buff = 20
+    val buffInFraction = buff.toFloat() / 100
+    val bonusIronOre = ironOre * buffInFraction
+    val bonusCrystalOre = crystalOre * buffInFraction
 
     println("Бонусная железная руда: " + String.format("%.0f", bonusIronOre))
     println("Бонусная кристаллическая руда: " + String.format("%.0f", bonusCrystalOre))
