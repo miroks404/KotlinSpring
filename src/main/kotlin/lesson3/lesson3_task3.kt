adee7af529553d469a2994c4fcb71e53d1bfec40
miroks404 <themiroks@gmail.com>
2024-10-04 14:03:20 +0200

    KS-3-3 corrections const names

--- src/main/kotlin/lesson3/lesson3_task3.kt
+++ src/main/kotlin/lesson3/lesson3_task3.kt
@@ -4,25 +4,25 @@ fun main() {
     val number = 8
     println(
         """
-        $number x 1 = ${number * one}
-        $number x 2 = ${number * two}
-        $number x 3 = ${number * three}
-        $number x 4 = ${number * four}
-        $number x 5 = ${number * five}
-        $number x 6 = ${number * six}
-        $number x 7 = ${number * seven}
-        $number x 8 = ${number * eight}
-        $number x 9 = ${number * nine}
+        $number x 1 = ${number * ONE}
+        $number x 2 = ${number * TWO}
+        $number x 3 = ${number * THREE}
+        $number x 4 = ${number * FOUR}
+        $number x 5 = ${number * FIVE}
+        $number x 6 = ${number * SIX}
+        $number x 7 = ${number * SEVEN}
+        $number x 8 = ${number * EIGHT}
+        $number x 9 = ${number * NINE}
     """.trimIndent()
     )
 }
 
-const val one: Byte = 1
-const val two: Byte = 2
-const val three: Byte = 3
-const val four: Byte = 4
-const val five: Byte = 5
-const val six: Byte = 6
-const val seven: Byte = 7
-const val eight: Byte = 8
-const val nine: Byte = 9
+const val ONE: Byte = 1
+const val TWO: Byte = 2
+const val THREE: Byte = 3
+const val FOUR: Byte = 4
+const val FIVE: Byte = 5
+const val SIX: Byte = 6
+const val SEVEN: Byte = 7
+const val EIGHT: Byte = 8
+const val NINE: Byte = 9
