7ec1c87f8595080cc7d228b04e1b33c23afeb1ca
miroks404 <themiroks@gmail.com>
2024-10-04 12:56:41 +0200

    KS-2-5 corrections

--- src/main/kotlin/lesson2/lesson2_task5.kt
+++ src/main/kotlin/lesson2/lesson2_task5.kt
@@ -4,9 +4,9 @@ import kotlin.math.pow
 
 fun main() {
     val depositAmount = 70000
-    val percentPerYear = 0.167
+    var percentPerYear = 16.7
     val durationOfDeposit = 20
-
+    percentPerYear /= PERCENTAGES_TO_FRACTIONS
     val resultDepositAmount =
         depositAmount.toDouble() * (NUMBER_FOR_COMPOUND_INTEREST + percentPerYear).pow(durationOfDeposit)
 
@@ -14,3 +14,4 @@ fun main() {
 }
 
 const val NUMBER_FOR_COMPOUND_INTEREST = 1
+const val PERCENTAGES_TO_FRACTIONS = 100
