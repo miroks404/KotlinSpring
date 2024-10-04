89921d42c2ad7132cd545dce7c1ad405123e4569
miroks404 <themiroks@gmail.com>
2024-10-04 13:59:56 +0200

    KS-3-5 corrections const names

--- src/main/kotlin/lesson3/lesson3_task5.kt
+++ src/main/kotlin/lesson3/lesson3_task5.kt
@@ -3,9 +3,9 @@ package org.example.lesson3
 fun main() {
     val move = "D2-D4;0"
     val parse = move.split("-", ";")
-    val where = parse[WHERE]
-    val from = parse[FROM]
-    val moveNumber = parse[MOVE_NUMBER]
+    val where = parse[WHERE_INDEX]
+    val from = parse[FROM_INDEX]
+    val moveNumber = parse[MOVE_NUMBER_INDEX]
 
     println(
         """
@@ -16,6 +16,6 @@ fun main() {
     )
 }
 
-const val WHERE = 0
-const val FROM = 1
-const val MOVE_NUMBER = 2
+const val WHERE_INDEX = 0
+const val FROM_INDEX = 1
+const val MOVE_NUMBER_INDEX = 2
