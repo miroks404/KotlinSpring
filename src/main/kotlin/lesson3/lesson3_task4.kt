caa6292c0dbeec032ae937a861d0855281d4b1b4
miroks404 <themiroks@gmail.com>
2024-10-04 13:00:09 +0200

    KS-3-4 corrections

--- src/main/kotlin/lesson3/lesson3_task4.kt
+++ src/main/kotlin/lesson3/lesson3_task4.kt
@@ -2,23 +2,14 @@ package org.example.lesson3
 
 fun main() {
     var where = "E2"
-    var whereIndex = where[NUMBER].digitToInt()
-    var move = 2
-    move += whereIndex
-    var from = where[LETTER] + move.toString()
+    var from = "E4"
     var moveNumber = 1
 
     println("$where-$from;$moveNumber")
 
     where = "D2"
-    whereIndex = where[NUMBER].digitToInt()
-    move = 1
-    move += whereIndex
-    from = where[LETTER] + move.toString()
+    from = "D3"
     moveNumber++
 
     println("$where-$from;$moveNumber")
 }
-
-const val LETTER = 0
-const val NUMBER = 1
