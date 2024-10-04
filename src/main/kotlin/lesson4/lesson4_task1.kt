c43335f4dbdb367c3e81ae53fe450641bf901da2
miroks404 <themiroks@gmail.com>
2024-10-04 13:57:43 +0200

    KS-4-1 added const val

--- src/main/kotlin/lesson4/lesson4_task1.kt
+++ src/main/kotlin/lesson4/lesson4_task1.kt
@@ -3,8 +3,9 @@ package org.example.lesson4
 fun main() {
     val bookedToday: Short = 13
     val bookedTomorrow: Short = 9
-    val allTables: Short = 13
 
-    println("Доступность столиков на сегодня: ${bookedToday !== allTables} ")
-    println("Доступность столиков на завтра: ${bookedTomorrow !== allTables}")
+    println("Доступность столиков на сегодня: ${bookedToday !== ALL_TABLES} ")
+    println("Доступность столиков на завтра: ${bookedTomorrow !== ALL_TABLES}")
 }
+
+const val ALL_TABLES: Short = 13
