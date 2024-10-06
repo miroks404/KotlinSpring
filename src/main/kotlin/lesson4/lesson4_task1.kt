fc4ff58f20bf7468a953efd418c33dc823b91858
miroks404 <themiroks@gmail.com>
2024-10-06 13:35:43 +0200

    KS-4-1 corrections

--- src/main/kotlin/lesson4/lesson4_task1.kt
+++ src/main/kotlin/lesson4/lesson4_task1.kt
@@ -4,8 +4,8 @@ fun main() {
     val bookedToday: Short = 13
     val bookedTomorrow: Short = 9
 
-    println("Доступность столиков на сегодня: ${bookedToday !== ALL_TABLES} ")
-    println("Доступность столиков на завтра: ${bookedTomorrow !== ALL_TABLES}")
+    println("Доступность столиков на сегодня: ${bookedToday < ALL_TABLES} ")
+    println("Доступность столиков на завтра: ${bookedTomorrow < ALL_TABLES}")
 }
 
 const val ALL_TABLES: Short = 13
