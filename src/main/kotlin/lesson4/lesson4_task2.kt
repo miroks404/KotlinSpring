cc713f58c81dec2373159baac3992fd6be1016b2
miroks404 <themiroks@gmail.com>
2024-10-06 13:44:20 +0200

    KS-4-2 corrections

--- src/main/kotlin/lesson4/lesson4_task2.kt
+++ src/main/kotlin/lesson4/lesson4_task2.kt
@@ -1,21 +1,27 @@
 package org.example.lesson4
 
 fun main() {
-    val average = true
+    val isAverage = true
     var weightAverageInKilograms = 20
     var volumeAverageInLiters = 0
 
-    println("Груз с весом $weightAverageInKilograms кг и объемом $volumeAverageInLiters л соответствует категории 'Average': ${average == (weightAverageInKilograms > MIN_KILOGRAMS && weightAverageInKilograms <= MAX_KILOGRAMS) and (volumeAverageInLiters < MAX_LITERS)}")
+    println("Груз с весом $weightAverageInKilograms кг и объемом $volumeAverageInLiters л соответствует категории" +
+            " 'Average': ${isAverage == (weightAverageInKilograms > MIN_KILOGRAMS &&
+                    weightAverageInKilograms <= MAX_KILOGRAMS) && (volumeAverageInLiters < MAX_LITERS)}")
 
     weightAverageInKilograms = 50
     volumeAverageInLiters = 80
 
-    println("Груз с весом $weightAverageInKilograms кг и объемом $volumeAverageInLiters л соответствует категории 'Average': ${average == (weightAverageInKilograms > MIN_KILOGRAMS && weightAverageInKilograms <= MAX_KILOGRAMS) and (volumeAverageInLiters < MAX_LITERS)}")
+    println("Груз с весом $weightAverageInKilograms кг и объемом $volumeAverageInLiters л соответствует категории " +
+            "'Average': ${isAverage == (weightAverageInKilograms > MIN_KILOGRAMS &&
+                    weightAverageInKilograms <= MAX_KILOGRAMS) && (volumeAverageInLiters < MAX_LITERS)}")
 
     weightAverageInKilograms = 0
     volumeAverageInLiters = 100
 
-    println("Груз с весом $weightAverageInKilograms кг и объемом $volumeAverageInLiters л соответствует категории 'Average': ${average == (weightAverageInKilograms > MIN_KILOGRAMS && weightAverageInKilograms <= MAX_KILOGRAMS) and (volumeAverageInLiters < MAX_LITERS)}")
+    println("Груз с весом $weightAverageInKilograms кг и объемом $volumeAverageInLiters л соответствует категории" +
+            " 'Average': ${isAverage == (weightAverageInKilograms > MIN_KILOGRAMS && 
+                    weightAverageInKilograms <= MAX_KILOGRAMS) && (volumeAverageInLiters < MAX_LITERS)}")
 }
 
 const val MIN_KILOGRAMS = 35
