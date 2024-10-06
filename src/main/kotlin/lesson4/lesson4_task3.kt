9c55b5c689dafcd374552a761bb2599fc20ff984
miroks404 <themiroks@gmail.com>
2024-10-06 14:09:52 +0200

    KS-4-3 corrections

--- src/main/kotlin/lesson4/lesson4_task3.kt
+++ src/main/kotlin/lesson4/lesson4_task3.kt
@@ -1,17 +1,18 @@
 package org.example.lesson4
 
 fun main() {
-    val sunnyWeather = true
-    val awningIsOpen = true
+    val isSunnyWeather = true
+    val isAwningOpen = true
     val airHumidityInPercentages = 20
     val season = "winter"
-    val favorableConditions: Boolean =
-        (sunnyWeather == FAVORABLE_SUNNY_WEATHER) and (awningIsOpen == FAVORABLE_AWING_IS_OPEN) and (airHumidityInPercentages == FAVORABLE_AIR_HUMIDITY) and (season !== UNFAVORABLE_SEASON)
+    val favorableConditions =
+        (isSunnyWeather == IS_FAVORABLE_SUNNY_WEATHER) && (isAwningOpen == IS_FAVORABLE_AWING_IS_OPEN) &&
+                (airHumidityInPercentages == FAVORABLE_AIR_HUMIDITY) && (season != UNFAVORABLE_SEASON)
 
     println("Благоприятные ли условия сейчас для роста бобовых? $favorableConditions")
 }
 
 const val FAVORABLE_AIR_HUMIDITY = 20
-const val FAVORABLE_SUNNY_WEATHER = true
-const val FAVORABLE_AWING_IS_OPEN = true
+const val IS_FAVORABLE_SUNNY_WEATHER = true
+const val IS_FAVORABLE_AWING_IS_OPEN = true
 const val UNFAVORABLE_SEASON = "winter"
