12c632c5915272189d6ccaa40ce852cedf448f72
miroks404 <themiroks@gmail.com>
2024-10-06 14:57:33 +0200

    KS-4-5 corrections

--- src/main/kotlin/lesson4/lesson4_task5.kt
+++ src/main/kotlin/lesson4/lesson4_task5.kt
@@ -1,17 +1,18 @@
 package org.example.lesson4
 
 fun main() {
-    val damageToTheShip = false
+    val isDamageToTheShip = false
     val numberOfCrew = 60
     val boxesOfProvision = 70
-    val weather = true
+    val isFavorableWeather = true
     val longTimeSwimming =
-        ((damageToTheShip !== SHIP_IS_DAMAGE) and (numberOfCrew >= MIN_CREW && numberOfCrew <= MAX_CREW) and (boxesOfProvision > MIN_BOXES_OF_PROVISIONS)) or ((numberOfCrew == MAX_CREW) and (weather == FAVORABLE_WEATHER) and (boxesOfProvision >= MIN_BOXES_OF_PROVISIONS))
+        ((isDamageToTheShip != IS_SHIP_IS_DAMAGE) && (numberOfCrew >= MIN_CREW && numberOfCrew <= MAX_CREW) &&
+                (boxesOfProvision > MIN_BOXES_OF_PROVISIONS)) || ((numberOfCrew == MAX_CREW) &&
+                (isFavorableWeather == true) && (boxesOfProvision >= MIN_BOXES_OF_PROVISIONS))
     println("Может ли корабль отправиться в плавание? $longTimeSwimming")
 }
 
-const val SHIP_IS_DAMAGE = true
+const val IS_SHIP_IS_DAMAGE = true
 const val MIN_CREW = 55
 const val MAX_CREW = 70
 const val MIN_BOXES_OF_PROVISIONS = 50
-const val FAVORABLE_WEATHER = true
