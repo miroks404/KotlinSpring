ed9ffdef9d41d39ad7a07d38052c6128f4aeb8ee
miroks404 <themiroks@gmail.com>
2024-10-07 12:47:45 +0200

    KS-4-4 correction (delete unnecessary variables)

--- src/main/kotlin/lesson4/lesson4_task4.kt
+++ src/main/kotlin/lesson4/lesson4_task4.kt
@@ -1,19 +1,15 @@
 package org.example.lesson4
 
 fun main() {
-    val hands = true
-    val legs = true
-    val back = true
-    val abs = true
     val day = 5
     val isEvenDay = day % EVEN_OR_ODD == 0
 
     println(
         """
-        Упражнения для рук: ${hands == !isEvenDay}
-        Упражнения для ног: ${legs == isEvenDay}
-        Упражнения для спины: ${back == isEvenDay}
-        Упражнения для пресса: ${abs == !isEvenDay}
+        Упражнения для рук: ${!isEvenDay}
+        Упражнения для ног: ${isEvenDay}
+        Упражнения для спины: ${isEvenDay}
+        Упражнения для пресса: ${!isEvenDay}
     """.trimIndent()
     )
 }
