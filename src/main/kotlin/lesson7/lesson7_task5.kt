8ed0725db97729002203bca8b4dab4c15ef4c8fb
miroks404 <themiroks@gmail.com>
2024-10-14 14:27:31 +0200

    KS-7-5 correction random password

--- src/main/kotlin/lesson7/lesson7_task5.kt
+++ src/main/kotlin/lesson7/lesson7_task5.kt
@@ -1,20 +1,24 @@
 package org.example.lesson7
 
+
 fun main() {
-    var password = ""
-    val passwordContect = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    val listOfPassword = mutableListOf<Char>()
+    val allChars = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+
+    listOfPassword.add(allChars.random())
+    listOfPassword.add(allChars.random())
+    listOfPassword.add(allChars.random())
 
     print("Введите длину пароля (минимум 6 символов): ")
     val passwordLenght = readln().toInt()
 
-    for (i in 1..6) {
-        password += passwordContect.random()
+    for (i in 4..passwordLenght) {
+        listOfPassword.add(allChars.random())
     }
-    for (i in MINIMUM_PASSWORD_LENGHT until passwordLenght) {
-        password += passwordContect.random()
-    }
+
+    listOfPassword.shuffle()
+
+    val password = listOfPassword.joinToString("")
 
     println(password)
 }
-
-const val MINIMUM_PASSWORD_LENGHT = 6
