3bc5932325b6907e5ca31bffcb8441db00ae4fdf
miroks404 <themiroks@gmail.com>
2024-10-16 16:02:19 +0200

    KS-10-2 register login & password

---
+++ src/main/kotlin/lesson10/lesson10_task2.kt
@@ -0,0 +1,17 @@
+package org.example.lesson10
+
+fun main() {
+    print("Введите логин: ")
+    val inputLogin = readln()
+
+    print("Введите пароль: ")
+    val inputPassword = readln()
+
+    println(validationLoginAndPassword(inputLogin, inputPassword))
+}
+
+fun validationLoginAndPassword(inputLogin: String, inputPassword: String): String = if
+        (inputLogin.length < MINIMUM_LENGTH || inputPassword.length < MINIMUM_LENGTH)
+        { "Логин или пароль недостаточно длинные" } else "Добро пожаловать в систему!"
+
+const val MINIMUM_LENGTH = 4
