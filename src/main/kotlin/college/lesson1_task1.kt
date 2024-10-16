27e57ac7b09d53bd943de08575e6afe87a0ca30c
miroks404 <themiroks@gmail.com>
2024-10-16 15:05:00 +0200

    C-1-1 upgrade code

--- src/main/kotlin/college/lesson1_task1.kt
+++ src/main/kotlin/college/lesson1_task1.kt
@@ -4,8 +4,7 @@ import kotlin.math.pow
 import kotlin.math.sqrt
 
 fun main() {
-    try { // Хотел написать только блок с readln, но эти переменные стали локальными, пришлось растянуть на весь код чтобы я мог пользоваться переменными a,b,c
-
+    val (a, b, c) = try { // Использовал это, потому что если взять с null типами, то очень тяжело потом делать сравнения
         print("Введите первую сторону треугольника: ")
         val a = readln().toDouble()
 
@@ -15,34 +14,41 @@ fun main() {
         print("Введите третью сторону треугольника: ")
         val c = readln().toDouble()
 
-        if (a <= ZERO_VARIABLE || b <= ZERO_VARIABLE || c <= ZERO_VARIABLE) {
-            println("У треугольника не может быть сторон <= 0")
-            System.exit(0)
-        }
-
-        if (a + b <= c || a + c <= b || c + b <= a) {
-            println("Сумма двух сторон СУЩЕСТВУЮЩЕГО треугольника должна быть больше третьей стороны")
-            System.exit(0)
-        }
-
-        val semiperimeter = (a + b + c) / 2
-
-        val area = sqrt(semiperimeter * (semiperimeter - a) * (semiperimeter - b) * (semiperimeter - c))
-
-        val typeOfTriangle: String
-
-        if (a == b || b == c) typeOfTriangle = "равнобедренного"
-        else if (a.pow(2.0) == b.pow(2.0) + c.pow(2.0) || b.pow(2.0) == a.pow(2.0) + c.pow(2.0) ||
-            c.pow(2.0) == a.pow(2.0) + b.pow(2.0)
-        ) typeOfTriangle = "прямоугольного"
-        else typeOfTriangle = "разностороннего"
-
-        println("Площадь $typeOfTriangle треугольника равна: ${"%.2f".format(area)} ")
-
+        Triple(a, b, c)
     } catch (e: Exception) {
         println("Вводите только числа!")
-        System.exit(0)
+        return
     }
+
+    val isRightTriangle = if (a <= ZERO_VARIABLE || b <= ZERO_VARIABLE || c <= ZERO_VARIABLE) {
+        println("У треугольника не может быть сторон <= 0")
+        false
+    } else if (a + b <= c || a + c <= b || c + b <= a) {
+        println("Сумма двух сторон СУЩЕСТВУЮЩЕГО треугольника должна быть больше третьей стороны")
+        false
+    } else true
+
+    if (!isRightTriangle) {
+        return
+    }
+
+    val semiPerimeter = (a + b + c) / 2
+
+    val area = sqrt(semiPerimeter * (semiPerimeter - a) * (semiPerimeter - b) * (semiPerimeter - c))
+
+    val typeOfTriangle = when {
+        a == b || b == c -> ISOSCELES_TRIANGLE
+        a.pow(2.0) == b.pow(2.0) + c.pow(2.0) ||
+                b.pow(2.0) == a.pow(2.0) + c.pow(2.0) || c.pow(2.0) == a.pow(2.0) + b.pow(2.0) ->
+            RIGHT_TRIANGLE
+
+        else -> VERSATILE_TRIANGLE
+    }
+
+    println("Площадь $typeOfTriangle треугольника равна: ${"%.2f".format(area)} ")
 }
 
 const val ZERO_VARIABLE: Double = 0.0
+const val VERSATILE_TRIANGLE = "разостороннего"
+const val ISOSCELES_TRIANGLE = "равнобедренного"
+const val RIGHT_TRIANGLE = "прямоугольного"
