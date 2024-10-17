f464fffa4ea32bbf2a922a96aa98401856c531d5
miroks404 <themiroks@gmail.com>
2024-10-17 02:08:49 +0200

    C-1-1 correct

--- src/main/kotlin/college/lesson1_task1.kt
+++ src/main/kotlin/college/lesson1_task1.kt
@@ -4,7 +4,7 @@ import kotlin.math.pow
 import kotlin.math.sqrt
 
 fun main() {
-    val (a, b, c) = try { // Использовал это, потому что если взять с null типами, то очень тяжело потом делать сравнения
+    val (a, b, c) = try {
         print("Введите первую сторону треугольника: ")
         val a = readln().toDouble()
 
@@ -20,35 +20,45 @@ fun main() {
         return
     }
 
-    val isRightTriangle = if (a <= ZERO_VARIABLE || b <= ZERO_VARIABLE || c <= ZERO_VARIABLE) {
-        println("У треугольника не может быть сторон <= 0")
-        false
-    } else if (a + b <= c || a + c <= b || c + b <= a) {
-        println("Сумма двух сторон СУЩЕСТВУЮЩЕГО треугольника должна быть больше третьей стороны")
-        false
-    } else true
+    val isTriangleExists = isTriangleExists(a, b, c)
 
-    if (!isRightTriangle) {
-        return
-    }
+    if (!isTriangleExists) return
 
-    val semiPerimeter = (a + b + c) / 2
+    val semiPerimeter = getSemiPerimeter(a, b, c)
 
-    val area = sqrt(semiPerimeter * (semiPerimeter - a) * (semiPerimeter - b) * (semiPerimeter - c))
+    val area = getArea(a, b, c, semiPerimeter)
 
-    val typeOfTriangle = when {
-        a == b || b == c -> ISOSCELES_TRIANGLE
-        a.pow(2.0) == b.pow(2.0) + c.pow(2.0) ||
-                b.pow(2.0) == a.pow(2.0) + c.pow(2.0) || c.pow(2.0) == a.pow(2.0) + b.pow(2.0) ->
-            RIGHT_TRIANGLE
+    val isIsoscelesTriangle = isIsoscelesTriangle(a, b, c)
+    val isRightTriangle = isRightTriangle(a, b, c)
 
-        else -> VERSATILE_TRIANGLE
-    }
+    val typeOfTriangle = if (isIsoscelesTriangle) ISOSCELES_TRIANGLE else if (isRightTriangle) RIGHT_TRIANGLE else
+        VERSATILE_TRIANGLE
 
     println("Площадь $typeOfTriangle треугольника равна: ${"%.2f".format(area)} ")
 }
 
+fun isTriangleExists(a: Double, b: Double, c: Double): Boolean {
+    if (a <= ZERO_VARIABLE || b <= ZERO_VARIABLE || c <= ZERO_VARIABLE) {
+        println("У треугольника не может быть сторон <= 0")
+        return false
+    } else if (a + b <= c || a + c <= b || c + b <= a) {
+        println("Сумма двух сторон СУЩЕСТВУЮЩЕГО треугольника должна быть больше третьей стороны")
+        return false
+    } else return true
+}
+
+fun isIsoscelesTriangle(a: Double, b: Double, c: Double): Boolean = a == b || b == c
+
+fun isRightTriangle(a: Double, b: Double, c: Double): Boolean = a.pow(2.0) == b.pow(2.0) + c.pow(2.0) ||
+        b.pow(2.0) == a.pow(2.0) + c.pow(2.0) || c.pow(2.0) == a.pow(2.0) + b.pow(2.0)
+
+fun getSemiPerimeter(a: Double, b: Double, c: Double): Double = (a + b + c) / 2
+
+fun getArea(a: Double, b: Double, c: Double, semiPerimeter: Double): Double =
+    sqrt(semiPerimeter * (semiPerimeter - a) * (semiPerimeter - b) * (semiPerimeter - c))
+
+
 const val ZERO_VARIABLE: Double = 0.0
-const val VERSATILE_TRIANGLE = "разостороннего"
+const val VERSATILE_TRIANGLE = "разностороннего"
 const val ISOSCELES_TRIANGLE = "равнобедренного"
 const val RIGHT_TRIANGLE = "прямоугольного"
