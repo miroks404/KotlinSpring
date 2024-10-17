10b9a09e5b0f65fbaef608aa12ae7ff1a8ad69f8
miroks404 <themiroks@gmail.com>
2024-10-17 02:31:58 +0200

    C-1-2 correct

--- src/main/kotlin/college/lesson1_task2.kt
+++ src/main/kotlin/college/lesson1_task2.kt
@@ -4,8 +4,7 @@ import kotlin.math.pow
 import kotlin.math.sqrt
 
 fun main() {
-    try { // Хотел написать только блок с readln, но эти переменные стали локальными, пришлось растянуть на весь код чтобы я мог пользоваться переменными a,b,c
-
+    val (a, b, c) = try {
         print("Введите первую сторону треугольника: ")
         val a = readln().toDouble()
 
@@ -15,51 +14,53 @@ fun main() {
         print("Введите третью сторону треугольника: ")
         val c = readln().toDouble()
 
-        if (a <= ZERO_VARIABLE || b <= ZERO_VARIABLE || c <= ZERO_VARIABLE) {
-            println("У треугольника не может быть сторон <= 0")
-            System.exit(0)
-        }
-
-        if (a + b <= c || a + c <= b || c + b <= a) {
-            println("Сумма двух сторон СУЩЕСТВУЮЩЕГО треугольника должна быть больше третьей стороны")
-            System.exit(0)
-        }
-
-        val semiperimeter = (a + b + c) / 2
-
-        val area = sqrt(semiperimeter * (semiperimeter - a) * (semiperimeter - b) * (semiperimeter - c))
-
-        val typeOfTriangle: String
-
-        val largestSide = maxOf(a, b, c)
-        var firstSide = 0.0
-        var secondSide = 0.0
-
-        if (largestSide == a && largestSide == b && largestSide == c) { // Определяю оставшиеся стороны
-            println("Площадь остроугольного треугольника равна: ${"%.2f".format(area)}")
-            System.exit(0)
-        } else if (largestSide == a) {
-            firstSide = b
-            secondSide = c
-        } else if (largestSide == b) {
-            firstSide = a
-            secondSide = c
-        } else if (largestSide == c) {
-            firstSide = a
-            secondSide = b
-        }
-
-        if (largestSide.pow(2.0) == firstSide.pow(2.0) + secondSide.pow(2.0)) typeOfTriangle = "прямоугольного"
-        else if (largestSide.pow(2.0) > firstSide.pow(2.0) + secondSide.pow(2.0))
-            typeOfTriangle = "тупоугольного"
-        else typeOfTriangle = "остроугольного"
-
-        println("Площадь $typeOfTriangle треугольника равна: ${"%.2f".format(area)}")
-
+        Triple(a, b, c)
     } catch (e: Exception) {
         println("Вводите только числа!")
+        return
+    }
+
+    val isTriangleExists = isTriangleExists(a, b, c)
+    if (!isTriangleExists) return
+
+    val semiPerimeter = getSemiPerimeter(a, b, c)
+
+    val area = getArea(a, b, c, semiPerimeter)
+
+    if (a == b && b == c) {
+        println("Площадь остроугольного треугольника равна: ${"%.2f".format(area)}")
         System.exit(0)
     }
+
+    val largestSide = maxOf(a, b, c)
+    val (firstSide, secondSide) = when (largestSide) {
+        a -> b to c
+        b -> a to c
+        else -> a to b
+    }
+
+    val typeOfTriangle = checkTriangleType(largestSide, firstSide, secondSide)
+
+    println("Площадь $typeOfTriangle треугольника равна: ${"%.2f".format(area)}")
+
 }
 
-const val ZERO_VARIABLE: Double = 0.0
+fun isTriangleExists(a: Double, b: Double, c: Double): Boolean {
+    if (a <= 0 || b <= 0 || c <= 0) {
+        println("У треугольника не может быть сторон <= 0")
+        return false
+    } else if (a + b <= c || a + c <= b || c + b <= a) {
+        println("Сумма двух сторон СУЩЕСТВУЮЩЕГО треугольника должна быть больше третьей стороны")
+        return false
+    } else return true
+}
+
+fun getSemiPerimeter(a: Double, b: Double, c: Double): Double = (a + b + c) / 2
+
+fun getArea(a: Double, b: Double, c: Double, semiPerimeter: Double): Double =
+    sqrt(semiPerimeter * (semiPerimeter - a) * (semiPerimeter - b) * (semiPerimeter - c))
+
+fun checkTriangleType(largestSide: Double, firstSide: Double, secondSide: Double): String =
+    if (largestSide.pow(2.0) == firstSide.pow(2.0) + secondSide.pow(2.0)) "прямоугольного"
+    else if (largestSide.pow(2.0) > firstSide.pow(2.0) + secondSide.pow(2.0)) "тупоугольного"
+    else "остроугольного"
