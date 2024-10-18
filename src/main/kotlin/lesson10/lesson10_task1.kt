54e72cf447d1d544c5b6d59ecfcd8ebf534d85a9
miroks404 <themiroks@gmail.com>
2024-10-18 19:24:48 +0200

    correct

--- src/main/kotlin/lesson10/lesson10_task1.kt
+++ src/main/kotlin/lesson10/lesson10_task1.kt
@@ -1,19 +1,19 @@
 package org.example.lesson10
 
-fun main() {
-    val humanNumber = rollTheDice()
-    val machineNumber = rollTheDice()
-
-    println("Число у вас: $humanNumber")
-    println("Число у машины: $machineNumber")
-
-    val result = if(humanNumber > machineNumber) "Победило человечество"
-        else if (humanNumber < machineNumber) "Победила машина"
-        else "Ничья!"
-
-    println(result)
-}
-
-fun rollTheDice() : Int = (1..6).random()
+//fun main() {
+//    val humanNumber = rollTheDice()
+//    val machineNumber = rollTheDice()
+//
+//    println("Число у вас: $humanNumber")
+//    println("Число у машины: $machineNumber")
+//
+//    val result = if(humanNumber > machineNumber) "Победило человечество"
+//        else if (humanNumber < machineNumber) "Победила машина"
+//        else "Ничья!"
+//
+//    println(result)
+//}
+//
+//fun rollTheDice() : Int = (1..6).random()
 
 
