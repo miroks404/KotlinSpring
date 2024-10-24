16e42baabff4ff636671b6df3fc0078a39bfbef8
miroks404 <themiroks@gmail.com>
2024-10-24 22:35:50 +0200

    KS-14-3 add filtering lists

--- src/main/kotlin/lesson14/lesson14_task3.kt
+++ src/main/kotlin/lesson14/lesson14_task3.kt
@@ -33,13 +33,13 @@ class Rectangle(
 }
 
 fun main() {
-    val circle1 = Circle("Белый", 3.14)
-    val circle2 = Circle("Черный", 5.0)
-    val circle3 = Circle("Белый", 2.0)
+    val circle1 = Circle(WHITE_COLOR, 3.14)
+    val circle2 = Circle(BLACK_COLOR, 5.0)
+    val circle3 = Circle(WHITE_COLOR, 2.0)
 
-    val rectangle1 = Rectangle("Белый", 10f, 3f)
-    val rectangle2 = Rectangle("Черный", 8f, 5f)
-    val rectangle3 = Rectangle("Черный", 11f, 8f)
+    val rectangle1 = Rectangle(WHITE_COLOR, 10f, 3f)
+    val rectangle2 = Rectangle(BLACK_COLOR, 8f, 5f)
+    val rectangle3 = Rectangle(BLACK_COLOR, 11f, 8f)
 
     val listOfCircles = listOf(circle1, circle2, circle3)
     val listOfRectangles = listOf(rectangle1, rectangle2, rectangle3)
@@ -47,13 +47,11 @@ fun main() {
     var sumAreasOfWhiteCircles = 0.0
     var sumPerimetersOfBlackRectangles = 0f
 
-    for (i in listOfCircles) {
-        if (i.color == WHITE_COLOR) sumAreasOfWhiteCircles += i.getArea()
-    }
+    val allWhiteCircles = listOfCircles.filter { it.color == WHITE_COLOR }
+    for (i in allWhiteCircles) sumAreasOfWhiteCircles += i.getArea()
 
-    for (i in listOfRectangles) {
-        if (i.color == BLACK_COLOR) sumPerimetersOfBlackRectangles += i.getPerimeter()
-    }
+    val allBlackRectangles = listOfRectangles.filter { it.color == BLACK_COLOR }
+    for (i in allBlackRectangles) sumPerimetersOfBlackRectangles += i.getPerimeter()
 
     println("Сумма площадей всех белых фигур: $sumAreasOfWhiteCircles")
     println("Сумма периметров всех черных фигур: $sumPerimetersOfBlackRectangles")
