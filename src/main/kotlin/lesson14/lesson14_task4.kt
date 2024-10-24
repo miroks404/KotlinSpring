a92a008eb3cfd03ec9e9fd06989751615f24a7b1
miroks404 <themiroks@gmail.com>
2024-10-24 03:10:59 +0200

    KS-14-4 corrections

--- src/main/kotlin/lesson14/lesson14_task4.kt
+++ src/main/kotlin/lesson14/lesson14_task4.kt
@@ -2,21 +2,42 @@ package org.example.lesson14
 
 abstract class CelestialBody(
     val planet: String,
-)
+) {
+    abstract fun addSputnik(nameOfSputnik: String)
+}
 
 class Planet(
     planet: String,
+    val listOfSputniks: MutableList<Sputnik> = mutableListOf(),
+) : CelestialBody(planet) {
+    override fun addSputnik(nameOfSputnik: String) {
+        val newSputnik = Sputnik(super.planet, nameOfSputnik)
+        listOfSputniks.add(newSputnik)
+    }
+}
+
+class Sputnik(
+    planet: String,
     val sputnik: String,
-    val listOfSputniks: List<String> = sputnik.split(",").toList()
-) : CelestialBody(planet)
+) : CelestialBody(planet) {
+    override fun addSputnik(nameOfSputnik: String) {
+        TODO("Not yet implemented")
+    }
+}
 
 fun main() {
-    val planet1 = Planet("Земля", "Меркурий, Марс, Индивид")
+    val planet1 = Planet("Земля")
+    val sputnik1 = planet1.addSputnik("Меркурий")
+    val sputnik2 = planet1.addSputnik("Луна")
 
-    println(
+    print(
         """
         -Название планеты: ${planet1.planet}
-        -Название спутников этой планеты: ${planet1.listOfSputniks.joinToString(", ")}
+        -Название спутников этой планеты: 
     """.trimIndent()
     )
+
+    for (i in planet1.listOfSputniks) {
+        print("${i.sputnik}, ")
+    }
 }
