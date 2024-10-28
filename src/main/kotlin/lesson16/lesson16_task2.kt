af7079f50b043f1c0597553677283a3f91aeffe4
miroks404 <themiroks@gmail.com>
2024-10-28 21:48:40 +0200

    KS-16-2 add pow

--- src/main/kotlin/lesson16/lesson16_task2.kt
+++ src/main/kotlin/lesson16/lesson16_task2.kt
@@ -1,11 +1,13 @@
 package org.example.lesson16
 
+import kotlin.math.pow
+
 private class Circle(
     private val radius: Double,
 ) {
     fun getCircumferenceLength() : Double = 2 * PI * radius
 
-    fun getArea() : Double = PI * radius * radius
+    fun getArea() : Double = PI * radius.pow(2)
 }
 
 fun main() {
