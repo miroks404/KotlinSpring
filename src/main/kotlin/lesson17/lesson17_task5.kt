1d14c3cec4846ebe26fbce36bc0a2886d887b582
miroks404 <themiroks@gmail.com>
2024-10-29 13:16:47 +0200

    KS-17-5 user information

---
+++ src/main/kotlin/lesson17/lesson17_task5.kt
@@ -0,0 +1,38 @@
+package org.example.lesson17
+
+private class User(
+    login: String,
+    password: String,
+) {
+    var login = login
+        set(value) {
+            field = value
+            println("Успешно изменен логин!")
+        }
+
+    var password = password
+        get() {
+            var protectedPassword = ""
+            for (i in field) {
+                protectedPassword += "*"
+            }
+            return protectedPassword
+        }
+        set(value) {
+            println("Вы не можете поменять пароль!")
+        }
+}
+
+fun main() {
+    val user1 = User("miroks404", "U1237")
+
+    user1.login = "zhenya"
+
+    println(user1.login)
+
+    println(user1.password)
+
+    user1.password = "123"
+
+    println(user1.password)
+}
