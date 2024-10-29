de7ea0f579cc1fa217d90cb7840944c315263bfd
miroks404 <themiroks@gmail.com>
2024-10-29 18:07:07 +0200

    KS-18-3 animals eating

---
+++ src/main/kotlin/lesson18/lesson18_task3.kt
@@ -0,0 +1,35 @@
+package org.example.lesson18
+
+private abstract class Animals {
+    open fun getEat() = ""
+}
+
+private class Fox(
+    private val name: String,
+) : Animals() {
+    override fun getEat(): String = "$name -> ест ягоды"
+}
+
+private class Dog(
+    private val name: String,
+) : Animals() {
+    override fun getEat(): String = "$name -> грызет кость"
+}
+
+private class Cat(
+    private val name: String,
+) : Animals() {
+    override fun getEat(): String = "$name -> ест рыбу"
+}
+
+fun main() {
+    val animal1 = Fox("Кристина")
+    val animal2 = Dog("Алена")
+    val animal3 = Cat("Оля")
+
+    val listOfAnimals: List<Animals> = listOf(animal1, animal2, animal3)
+
+    for (i in listOfAnimals) {
+        println(i.getEat())
+    }
+}
