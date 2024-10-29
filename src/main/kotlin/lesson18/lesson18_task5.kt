4ab7747bc24a4f6f7224d4ad680c4ff5c4acf55f
miroks404 <themiroks@gmail.com>
2024-10-29 19:26:55 +0200

    KS-18-5 corrections

--- src/main/kotlin/lesson18/lesson18_task5.kt
+++ src/main/kotlin/lesson18/lesson18_task5.kt
@@ -1,61 +1,75 @@
 package org.example.lesson18
 
 private abstract class Object {
-    abstract fun draw(): String
+    abstract fun draw()
 }
 
 private class CircleInt(
     private val xInt: Int,
     private val yInt: Int,
 ) : Object() {
-    override fun draw() = "Circle int"
+    override fun draw() {
+        println(xInt)
+    }
 }
 
 private class CircleFloat(
     private val xFloat: Float,
     private val yFloat: Float,
 ) : Object() {
-    override fun draw() = "Circle float"
+    override fun draw() {
+        println(xFloat)
+    }
 }
 
 private class SquareInt(
     private val xInt: Int,
     private val yInt: Int,
 ) : Object() {
-    override fun draw() = "Square int"
+    override fun draw() {
+        println(xInt)
+    }
 }
 
 private class SquareFloat(
     private val xFloat: Float,
     private val yFloat: Float,
 ) : Object() {
-    override fun draw() = "Square float"
+    override fun draw() {
+        println(xFloat)
+    }
 }
 
 private class DotInt(
     private val xInt: Int,
     private val yInt: Int,
 ) : Object() {
-    override fun draw() = "Dot int"
+    override fun draw() {
+        println(xInt)
+    }
 }
 
 private class DotFloat(
     private val xFloat: Float,
     private val yFloat: Float,
 ) : Object() {
-    override fun draw() = "Dot float"
+    override fun draw() {
+        println(xFloat)
+    }
 }
 
 private class Screen(
     private val figure: Object,
 ) : Object() {
-    override fun draw(): String = figure.draw()
+    override fun draw() {
+        figure.draw()
+    }
 }
 
 fun main() {
     val screen1 = Screen(DotInt(1,2))
-    println(screen1.draw())
+    screen1.draw()
 
     val screen2 = Screen(SquareFloat(3f,5f))
-    println(screen2.draw())
+    screen2.draw()
 }
