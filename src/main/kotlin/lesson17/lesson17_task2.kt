bd41d5e766d0881d9ac742830149279f71e48754
miroks404 <themiroks@gmail.com>
2024-10-29 12:29:59 +0200

    KS-17-2 name of ship

---
+++ src/main/kotlin/lesson17/lesson17_task2.kt
@@ -0,0 +1,22 @@
+package org.example.lesson17
+
+class Ship(
+    private val name: String,
+    val averageSpeed: Int,
+    val portOfResidence: String,
+) {
+    var nameOfShip = name
+        get() = "Название корабля: $field"
+        set(value) {
+            field = name
+            println("Название корабля нельзя поменять!")
+        }
+}
+
+fun main() {
+    val ship1 = Ship("Якорь", 25, "Калининград")
+
+    ship1.nameOfShip = "апрель"
+
+    println(ship1.nameOfShip)
+}
