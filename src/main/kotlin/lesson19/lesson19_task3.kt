4be43f1a482ad6a2084443d30119663d956e443f
miroks404 <themiroks@gmail.com>
2024-10-30 14:33:52 +0200

    KS-19-3 ship todo

---
+++ src/main/kotlin/lesson19/lesson19_task3.kt
@@ -0,0 +1,15 @@
+package org.example.lesson19
+
+private class Ship{
+    fun takeoff() {
+        TODO()
+    }
+
+    fun landing() {
+        TODO()
+    }
+
+    fun shootAtTheAsteroid() {
+        TODO("More info on logic")
+    }
+}
