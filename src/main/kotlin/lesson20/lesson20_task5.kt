b9ac1384b7c3a2cd232004efaad3b6d9e4112b7d
miroks404 <themiroks@gmail.com>
2024-10-31 00:12:02 +0200

    KS-20-5 modify say

---
+++ src/main/kotlin/lesson20/lesson20_task5.kt
@@ -0,0 +1,35 @@
+package org.example.lesson20
+
+class Robot(
+    private val listOfPhrases: List<String> = listOf(
+        "Бежит по телу сладостная дрожь",
+        "По той же все, нехоженой судьбе",
+        "Сдави и омой дрожащую кость",
+        "Белеют скверы и тропинки",
+        "Березка капельками света"
+    ),
+    var phrase: String = ""
+) {
+    fun say() {
+        phrase = listOfPhrases[listOfPhrases.indices.random()]
+        println(phrase)
+    }
+
+    fun setModifier(modifier: (String) -> String) {
+        phrase = modifier(phrase)
+    }
+
+    fun say(phrase: String) {
+        println(phrase)
+    }
+}
+
+fun main() {
+    val robot = Robot()
+
+    robot.say()
+
+    robot.setModifier { phrase -> "$phrase!" }
+
+    robot.say(robot.phrase)
+}
