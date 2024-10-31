69073e9db6a0a8cf548d1e06fc19ac6baca9a0d1
miroks404 <themiroks@gmail.com>
2024-10-31 22:14:23 +0200

    KS-22-4 MVVM

---
+++ src/main/kotlin/lesson22/lesson22_task4.kt
@@ -0,0 +1,28 @@
+package org.example.lesson22
+
+private class MainScreenViewModel {
+    fun loadData() {
+        val newData1 = MainScreenState("отсутствие данных")
+
+        val newData2 = MainScreenState("загрузка данных")
+        newData2.changeLoading(true)
+
+        val newData3 = MainScreenState("наличие загруженных данных")
+    }
+}
+
+private data class ViewModel(
+    val mainScreenState: MainScreenState,
+)
+
+
+private data class MainScreenState(
+    var data: String = "",
+    val isLoading: Boolean = false,
+) {
+    fun changeLoading(isLoading: Boolean) {
+        val newMainScreenState = this.copy(isLoading = isLoading)
+    }
+}
+
+
